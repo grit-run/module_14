9fce354d1f974935a4fc9b56ae98ebf57f44c0fb
elinsky <dmitry.ilinsky@gmail.com>
2024-09-14 19:09:35 +0300

    first SQL steps

---
+++ module_14_1.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+connection = sqlite3.connect("not_telegram.db")
+cursor = connection.cursor()
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS Users (
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)''')
+
+#for i in range(1,11):
+#    cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)",
+#                   ("User{}".format(i), "User{}@example.com".format(i), i*10, i*1000))
+
+#for i in range(1, 11, 2):
+#    cursor.execute("UPDATE Users SET balance = ? WHERE id = ?", (500, i))
+
+#for i in range(1, 11, 3):
+#    cursor.execute("DELETE FROM Users WHERE id = ?", (i,))
+
+#cursor.execute("SELECT username, email, age, balance FROM Users WHERE age != 60")
+#selected_users = cursor.fetchall()
+#for sl_usr in selected_users:
+#    print(sl_usr)
+
+connection.commit()
+connection.close()
